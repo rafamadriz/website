67de82f0384edca9e76fc80dd5cb77b4500bfc62
Rafael Madriz <rafa@rafaelmadriz.com>
2025-12-16 22:09:43 -0300

    feat: add basic templates and posts collection

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,14 @@ import { VentoPlugin } from "eleventy-plugin-vento"
 import sassPlugin from "./src/_11ty/sassPlugin.js"
 
 export default async function(eleventyConfig) {
+    // COLLECTIONS
+    eleventyConfig.addCollection("posts", function(CollectionApi) {
+        return CollectionApi
+            .getFilteredByGlob("./src/blog/**/*.md")
+            .filter(page => page.inputPath != "./src/blog/index.md")
+            .reverse()
+    })
+
     // PLUGINS
     eleventyConfig.addPlugin(sassPlugin),
     eleventyConfig.addPlugin(VentoPlugin) // recommended to load as the last plugin
