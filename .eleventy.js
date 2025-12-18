8843b54b80567340b96f98e77388a9ad4376f2c2
Rafael Madriz <rafa@rafaelmadriz.com>
2025-12-18 09:13:09 -0300

    fix: transform markdown relative links to their output URLs
    If I link to another post in markdown using a filesystem path.
    It doesn't get transformed to the correct output link. This
    plugin solves that.
    
    Ref:
    - https://www.11ty.dev/docs/plugins/inputpath-to-url/

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ import { VentoPlugin } from "eleventy-plugin-vento"
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img"
 import footnote_plugin from "markdown-it-footnote"
 import { IdAttributePlugin } from "@11ty/eleventy"
+import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
 
 // CUSTOM PLUGINS/UTILS
 import sassPlugin from "./src/_11ty/sassPlugin.js"
@@ -53,6 +54,7 @@ export default async function(eleventyConfig) {
     // PLUGINS
     eleventyConfig.addPlugin(sassPlugin),
     eleventyConfig.addPlugin(IdAttributePlugin)
+    eleventyConfig.addPlugin(InputPathToUrlTransformPlugin)
     eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(footnote_plugin))
     eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
         formats: [ "avif" ],
