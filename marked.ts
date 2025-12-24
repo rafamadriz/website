ea92ee4ef58b5bfe62721b03e628f5f960a34935
Rafael Madriz <rafa@rafaelmadriz.com>
2025-12-24 19:11:49 -0300

    feat: add footnotes marked plugin

--- marked.ts
+++ marked.ts
@@ -1,8 +1,14 @@
-import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js"
-import { gfmHeadingId } from "https://cdn.jsdelivr.net/npm/marked-gfm-heading-id@4.1.3/+esm"
-
+import markedFootnote from 'https://cdn.jsdelivr.net/npm/marked-footnote@1.4.0/+esm';
+import { gfmHeadingId } from "https://cdn.jsdelivr.net/npm/marked-gfm-heading-id@4.1.3/+esm";
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
 marked.use(gfmHeadingId())
+
+marked.use(markedFootnote({
+    footnoteDivider: true,
+    sectionClass: "footnotes prose"
+}))
+
 export class MarkedEngine implements Lume.Engine {
     render(content: string, _data: Record<string, unknown>, _filename: string) {
         return marked(content);
